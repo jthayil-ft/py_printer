7aad4df2b320928be97171ae36ae54bd5af269cf
Jones Thayil <jones.thayil@gmail.com>
2021-10-05 07:28:56 +0530

    Capital printer

--- printer.py
+++ printer.py
@@ -2,25 +2,24 @@ import os
 from time import sleep
 from threading import Thread
 
+exelist = ['printer.exe', 'Printer.exe', 'PRINTER.exe']
 
 def printer(path = os.getcwd()):
     for file in os.listdir(path):
-        if file not in ['printer.exe']:
+        if file not in exelist:
             file_path = os.path.join(path, file)
             if os.path.isfile(file_path):
                 os.startfile(file, "print")
                 sleep(9)
             else:
                 printer(file_path)
+    delete(path)
 
 def delete(path = os.getcwd()):
     for file in os.listdir(path):
-        if file not in ['printer.exe']:
+        if file not in exelist:
             file_path = os.path.join(path, file)
             if os.path.isfile(file_path):
                 os.remove(file_path)
-            else:
-                printer(file_path)
 
 printer()
-delete()
