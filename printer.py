22cd3f93032bebfd58517215b57ef74d154c0e73
Jones Thayil <jones.thayil@gmail.com>
2021-10-16 20:54:33 +0530

    new

--- printer.py
+++ printer.py
@@ -1,6 +1,5 @@
 import os
 from time import sleep
-from threading import Thread
 
 exelist = ['printer.exe', 'Printer.exe', 'PRINTER.exe']
 
@@ -9,17 +8,17 @@ def printer(path = os.getcwd()):
         if file not in exelist:
             file_path = os.path.join(path, file)
             if os.path.isfile(file_path):
-                os.startfile(file, "print")
-                sleep(9)
+                os.startfile(file_path, "print")
+                # sleep(9)
             else:
                 printer(file_path)
-    delete(path)
+    # delete(path)
 
-def delete(path = os.getcwd()):
-    for file in os.listdir(path):
-        if file not in exelist:
-            file_path = os.path.join(path, file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
+# def delete(path = os.getcwd()):
+#     for file in os.listdir(path):
+#         if file not in exelist:
+#             file_path = os.path.join(path, file)
+#             if os.path.isfile(file_path):
+#                 os.remove(file_path)
 
-printer()
+printer("C:\\SAP\\Digi_Sign\\Print")
